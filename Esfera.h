5f47a2399fded9a18a34636d8f158a547747fd21
fmenezes702 <62705573+fmenezes702@users.noreply.github.com>
2021-04-08 17:55:48 -0300

    Update Esfera.h

--- Esfera.h
+++ Esfera.h
@@ -4,10 +4,13 @@ class Esfera :
 	public Objeto
 {
 private:
+        vector<float> centro;
+	float R;
+	vector<vector<float>> material;
 	vector<float> getNormal(vector<float> ponto);
 
 public:
-	Esfera();
+	Esfera(vector<float> centro, float R, vector<vector<float>> material);
 	tuple<bool, float, vector<vector<float>>, vector<float>> hasIntersection(float p0x, float p0y, float p0z, float dx, float dy, float dz);
 };
 
